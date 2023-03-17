b10484d638459db85062cec02a8c5962dbbbb18b
Mukhtar Babiker <mukhtarbabiker@gmail.com>
2023-03-17 14:51:44 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A C program that prints the alphabets
+ * Return: value 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyx";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+	putchar(alp[i]);
+	}
+putchar('\n');
+return (0);
+}
