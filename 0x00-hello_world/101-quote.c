4cd2da5d6dc439f8a54804313fc9aa5dfb87d311
Mukhtar Babiker <mukhtarbabiker@gmail.com>
2023-03-16 21:40:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char str[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+puts(str);
 return (1);
 }
