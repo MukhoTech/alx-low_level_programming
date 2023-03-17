c385cc69766367ec464cf672c06f7a1db9eea20c
Mukhtar Babiker <mukhtarbabiker@gmail.com>
2023-03-16 18:33:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Using a function printf to print a code of line
+ * Return: value (0)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
