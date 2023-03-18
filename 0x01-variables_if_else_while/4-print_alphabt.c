ccfe92c39f6f2edbfaee2eb2edc3a51c2d8b4d2e
Mukhtar Babiker <mukhtarbabiker@gmail.com>
2023-03-17 18:31:05 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main  - A C program that prints
+*Return: Value 0 (Success)
+*/
+
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
