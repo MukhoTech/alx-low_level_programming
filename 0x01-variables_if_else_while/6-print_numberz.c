c54a6cbe73547379d676c9bf8e3ffc2f7c2c5f7c
Mukhtar Babiker <mukhtarbabiker@gmail.com>
2023-03-17 18:52:31 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - A C program that prints digit numbers of base 10
+*Return: Value 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+		putchar(x);
+	putchar('\n');
+
+	return (0);
+}
