c0993d6bd93081272a35245fe9d7d0b0608fe751
Mukhtar Babiker <mukhtarbabiker@gmail.com>
2023-03-22 18:07:48 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main: displays all the sum of multiples of 5 and 3 upto 1024
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, x = 0;
+
+	while (i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	x += i;
+	}
+	i++;
+	}
+	printf("%d\n", x);
+	return (0);
+}
