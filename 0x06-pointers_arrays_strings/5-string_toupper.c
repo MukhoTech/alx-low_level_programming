3cd6ded690e12b30377ad9f75df86ff0b574293c
Mukhtar Babiker <mukhtarbabiker@gmail.com>
2023-03-30 19:06:53 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lower case letters
+ * of string to uppercase
+ * @str: the string to be changed
+ * Return: A pointer to change string
+ */
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+	if (str[index] >= 'a' && str[index] <= 'z')
+	str[index] -= 32;
+	index++;
+	}
+	return (str);
+}
